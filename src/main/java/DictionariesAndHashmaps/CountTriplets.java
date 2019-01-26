b34b95a05ba3d7e2973e8962c131c39fcaf33576
Roman Gorbatenko <roman_gorbatenko@epam.com>
2019-01-26 19:08:54 +0300

    Dictionaries And HashMaps

---
+++ src/main/java/DictionariesAndHashmaps/CountTriplets.java
@@ -0,0 +1,31 @@
+package DictionariesAndHashmaps;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CountTriplets {
+
+  static long countTriplets(List<Long> arr, long r) {
+    Map<Long, Long> mapElements = new HashMap<>();
+    Map<Long, Long> mapTriplets = new HashMap<>();
+    // long r = 2;
+    long totalCount = 0;
+
+    for (int i = arr.size() - 1; i >= 0; i--) {
+      long element = arr.get(i);
+
+      if (mapTriplets.containsKey(element * r)) {
+        totalCount += mapTriplets.get(element * r);
+      }
+
+      if (mapElements.containsKey(element * r)) {
+        long value = mapElements.get(element * r);
+        mapTriplets.put(element, mapTriplets.getOrDefault(element, 0L) + value);
+      }
+
+      mapElements.put(element, mapElements.getOrDefault(element, 0L) + 1);
+    }
+    return totalCount;
+  }
+}
--- src/main/java/WarmUpChallenges/RepeatedString.java
+++ src/main/java/WarmUpChallenges/RepeatedString.java
@@ -4,16 +4,26 @@ public class RepeatedString {
 
   static long repeatedString(String s, long n) {
 
+    long factor = (n / s.length());
+    long rem = (n % s.length());
     long count = 0;
+    /*
+    Count number of a's in base string
+     */
     for (char c : s.toCharArray()) {
       if (c == 'a') {
         count++;
       }
     }
 
-    long factor = (n / s.length());
-    long rem = (n % s.length());
+    /*
+    multiply number of a's by number of repeatable strings
+     */
     count = factor * count;
+
+    /*
+    add a number of a's in the end of the string
+     */
     for (int i = 0; i < rem; i++) {
       if (s.charAt(i) == 'a') {
         count++;
